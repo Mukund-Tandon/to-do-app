0928b772c03f0719ed4b6189a786308f51045337
Mukund-Tandon <mukundtandon1211@gmail.com>
2021-04-03 21:52:18 +0530

    Started making app

---
+++ lib/task_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Taskscreen extends StatefulWidget {
+  @override
+  _TaskscreenState createState() => _TaskscreenState();
+}
+
+class _TaskscreenState extends State<Taskscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
